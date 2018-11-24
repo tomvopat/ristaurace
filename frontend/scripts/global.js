4923aefacf1e8d482786a8d000a85726c50177fa
Lukáš Chalupa <luchalupa@gmail.com>
2018-11-24 19:18:08 +0100

    link jquery + generovani denni nabidky pres REST

---
+++ frontend/scripts/global.js
@@ -0,0 +1,19 @@
+let createMenuItemHtml = function(item) {
+	let menuItemHtml =
+		'<li class=\"menuList-item\">'
+		+	'<label class="menuList-label" for="item-' + item.id + '">' + item.name + '</label>'
+		+	'<input type=\"number\" value=0 name=\"item-' + item.id + '\">'
+		+ '</li>';
+	return menuItemHtml;
+}
+
+$.ajax({ 
+	type: "GET",
+	url: "http://www.mocky.io/v2/5bf990203200006200f22560",
+	dataType: "jsonp",
+	success: function(data){
+		data.result.forEach(item => {
+			$("#daily-items").append(createMenuItemHtml(item));
+		});
+	}
+ });
