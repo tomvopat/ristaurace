8075bfede2195687b445bee053677f08dbdd17ed
Lukáš Chalupa <luchalupa@gmail.com>
2018-11-25 13:45:46 +0100

    generovani HTML objednavek z REST API

--- frontend/scripts/global.js
+++ frontend/scripts/global.js
@@ -1,14 +1,61 @@
 let createMenuItemHtml = function(item) {
 	let menuItemHtml =
 		'<li class=\"menuList-item\">'
-		+	'<label class="menuList-label" for="item-' + item.id + '">' + item.name + '</label>'
+		+	'<label class="menuList-label" for="item-' + item.id + '">' + item.name + ' (' + item.price + 'Kč)</label>'
 		+	'<input class="menuList-input" type=\"number\" value=0 min="0" max="100" name=\"item-' + item.id + '\">'
 		+ '</li>';
 	return menuItemHtml;
 }
 
 let createOrderItemHtml = function(item) {
+	let created = item.created;
+	let currentDate = new Date();
+	let creationDate = new Date(created.year, created.month - 1, created.day, created.hours, created.minutes);
+	let difference = Math.abs(currentDate - creationDate);
+	let differenceString = Math.floor(difference / 60000) + ':' + Math.ceil((difference % 60000) / 1000);
 	
+	let foodItemsHtml = '';
+	item.food.forEach((food) => {
+		foodItemsHtml = foodItemsHtml
+		+ '<li class="menuList-item">'
+		+	'<span class="ordersList-foodName">' + food.name + '</span>'
+		+	'<span class="ordersList-foodCount">' + food.amount + '</span>'
+		+ '</li>'
+	});
+
+	let drinkItemsHtml = '';
+	item.drinks.forEach((drink) => {
+		drinkItemsHtml = drinkItemsHtml
+		+ '<li class="menuList-item">'
+		+	'<span>' + drink.name + '</span>'
+		+	'<span>' + drink.amount + '</span>'
+		+ '</li>'
+	});
+	
+	let menuItemHtml =
+		'<li class="ordersList-item itemBox">'
+		+	'<h3 class="itemBox-heading">Stůl č. ' + item.table + '</h3>'
+		+	'<span class="ordersList-timer">' + differenceString + '</span>'
+		+	'<div class="ordersList-content">'
+		+		'<div class="ordersList-food">'
+		+			'<h4>Pokrmy</h4>'
+		+			'<ul class="menuList">'
+		+				foodItemsHtml
+		+			'</ul>'
+		+		'</div>'
+		+		'<div class="ordersList-drinks">'
+		+			'<h4>Nápoje</h4>'
+		+			'<ul class="menuList">'
+		+				drinkItemsHtml
+		+			'</ul>'
+		+		'</div>'
+		+	'</div>'
+		+	'<div class="itemBox-buttons">'
+		+		'<button class="button">Označit jako připravené</button>'
+		+		'<button class="button">Označit jako vyřízené</button>'
+		+	'</div>'
+		+ '</li>';
+	return menuItemHtml;
 }
 
 
@@ -41,6 +88,17 @@ $.ajax({
 	success: function(data){
 		data.result.forEach(item => {
 			$("#drinks").append(createMenuItemHtml(item));
+		});
+	}
+});
+
+$.ajax({ 
+	type: "GET",
+	url: "http://www.mocky.io/v2/5bfa90283200006000bee35a",
+	dataType: "jsonp",
+	success: function(data){
+		data.result.forEach(item => {
+			$("#ordersList").append(createOrderItemHtml(item));
 		});
 	}
 });
