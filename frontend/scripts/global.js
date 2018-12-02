9a853985aee95c083170121f4f6db58c1c2e77c4
Lukáš Chalupa <luchalupa@gmail.com>
2018-12-02 23:37:20 +0100

    propojovani s backendem (vytvoreni objednavky + vypis objednavek)

--- frontend/scripts/global.js
+++ frontend/scripts/global.js
@@ -1,3 +1,6 @@
+let baseUrl = "http://localhost:8080/"
+
+
 let timer = function() {
 	setInterval("increaseTimer()", 1000);
 }
@@ -18,11 +21,28 @@ let increaseTimer = function() {
 
 let createMenuItemHtml = function(item) {
 	let menuItemHtml =
-		'<li class="menuList-item">'
+		'<li data-id="' + item.id + '" class="menuList-item">'
 		+	'<label class="menuList-label" for="item-' + item.id + '">' + item.nazev + ' <span class="menuList-price">(' + item.cena + 'Kč)</span></label>'
 		+	'<input class="menuList-input" type="number" value=0 min="0" max="100" name="item-' + item.id + '">'
 		+ '</li>';
 	return menuItemHtml;
+}
+
+let getOrderItemsById = function(id, callback) {
+	let items = [];
+	$.ajax({
+		type: "GET",
+		url: baseUrl + "order/all/",
+		dataType: "json",
+		success: function(data){
+			data.forEach(item => {
+				if(item.ucetByIdUcet.id == id) {
+					items.push(item.polozkaMenuByIdPolozkaMenu);
+				}
+			});
+			callback(items);
+		}
+	});
 }
 
 let createOrderItemHtml = function(item) {
@@ -31,54 +51,54 @@ let createOrderItemHtml = function(item) {
 	let difference = Math.abs(currentDate - creationDate);
 	let differenceString = Math.floor(difference / 60000) + ':' + Math.ceil((difference % 60000) / 1000);
 	let foodItemsHtml = '';
-	/*item.food.forEach((food) => {
-		foodItemsHtml = foodItemsHtml
-		+ '<li class="menuList-item">'
-		+	'<span class="ordersList-foodName">' + food.name + '</span>'
-		+	'<span class="ordersList-foodCount">' + food.amount + 'x</span>'
-		+ '</li>'
-	});*/
-
-	let drinkItemsHtml = '';
-	/*item.drinks.forEach((drink) => {
-		drinkItemsHtml = drinkItemsHtml
-		+ '<li class="menuList-item">'
-		+	'<span>' + drink.name + '</span>'
-		+	'<span>' + drink.amount + 'x</span>'
-		+ '</li>'
-	});*/
+	getOrderItemsById(item.ucetByIdUcet.id, function(foodItems) {
+		foodItems.forEach((food) => {
+			foodItemsHtml = foodItemsHtml
+			+ '<li id="food-' + food.id + '" class="menuList-item">'
+			+	'<span class="ordersList-foodName">' + food.nazev + '</span>'
+			+	'<span class="ordersList-foodPrice"></span>'
+			+ '</li>'
+		});
 	
-	let menuItemHtml =
-		'<li class="ordersList-item itemBox" data-itemId="' + item.id + '">'
-		+	'<h3 class="itemBox-heading">Stůl č. ' + item.stulByIdStul.cisloStolu + '</h3>'
-		+	'<span class="ordersList-timer">' + differenceString + '</span>'
-		+	'<div class="ordersList-content">'
-		+		'<div class="ordersList-food">'
-		+			'<h4>Pokrmy</h4>'
-		+			'<ul class="menuList">'
-		+				foodItemsHtml
-		+			'</ul>'
-		+		'</div>'
-		+		'<div class="ordersList-drinks">'
-		+			'<h4>Nápoje</h4>'
-		+			'<ul class="menuList">'
-		+				drinkItemsHtml
-		+			'</ul>'
-		+		'</div>'
-		+	'</div>'
-		+	'<div class="itemBox-buttons">'
-		+		'<button class="button button-ready">Označit jako připravené</button>'
-		+		'<button class="button button-done">Označit jako vyřízené</button>'
-		+	'</div>'
-		+ '</li>';
-	return menuItemHtml;
+		let drinkItemsHtml = '';
+		/*item.drinks.forEach((drink) => {
+			drinkItemsHtml = drinkItemsHtml
+			+ '<li class="menuList-item">'
+			+	'<span>' + drink.name + '</span>'
+			+	'<span>' + drink.amount + 'x</span>'
+			+ '</li>'
+		});*/
+		
+		let menuItemHtml =
+			'<li class="ordersList-item itemBox" data-itemId="' + item.id + '">'
+			+	'<h3 class="itemBox-heading">Stůl č. ' + item.stulByIdStul.cisloStolu + '</h3>'
+			+	'<span class="ordersList-timer">' + differenceString + '</span>'
+			+	'<div class="ordersList-content">'
+			+		'<div class="ordersList-food">'
+			+			'<h4>Pokrmy</h4>'
+			+			'<ul class="menuList">'
+			+				foodItemsHtml
+			+			'</ul>'
+			+		'</div>'
+			+		'<div class="ordersList-drinks">'
+			+			'<h4>Nápoje</h4>'
+			+			'<ul class="menuList">'
+			+				drinkItemsHtml
+			+			'</ul>'
+			+		'</div>'
+			+	'</div>'
+			+	'<div class="itemBox-buttons">'
+			+		'<button class="button button-ready">Označit jako připravené</button>'
+			+		'<button class="button button-done">Označit jako vyřízené</button>'
+			+	'</div>'
+			+ '</li>';
+			$("#ordersList").append(menuItemHtml);
+	});
 }
 
 let createTableOptionHtml = function(item) {
 	return '<option class="table-option" value=' + item.id + '>' + item.cisloStolu + '</option>';
 }
-
-let baseUrl = "http://localhost:8080/"
 
 $.ajax({
 	type: "GET",
@@ -89,7 +109,7 @@ $.ajax({
 			$("#tableSelect").append(createTableOptionHtml(item));
 		});
 	}
- });
+});
 
 $.ajax({
 	type: "GET",
@@ -131,21 +151,37 @@ $.ajax({
 	dataType: "json",
 	success: function(data){
 		data.forEach(item => {
-			$("#ordersList").append(createOrderItemHtml(item));
+			createOrderItemHtml(item);
 		});
 	}
 });
 
 
-$(document).on('click', '#orderButton', function(event) {
+$(document).on('submit', '#orderForm', function(event) {
 	event.preventDefault();
 	const tableId = $("#tableSelect").val();
+	let orderId;
 	$.ajax({ 
 		type: "POST",
 		url: baseUrl + "bill/new/" + tableId,
 		dataType: "json",
-		success: function() {
-			console.log("success");
+		success: function(data) {
+			orderId = data.id;
+			let $allItems = $(".menuList-item");
+			$allItems.each((i, item) => {
+				const amount = $(item).find("input").val();
+				let itemId = ($(item).data("id"));
+				for (let index = 0; index < amount; index++) {
+					$.ajax({ 
+						type: "POST",
+						url: baseUrl + "menu-items/order/" + orderId + "/" + itemId,
+						dataType: "json",
+						success: function() {
+							console.log("here");
+						}
+					});
+				}
+			});
 		}
 	});
 });
