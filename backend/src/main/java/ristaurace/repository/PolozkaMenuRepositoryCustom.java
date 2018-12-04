4f96e80e66cf13a5d451048d724a9956e8d67d6f
Tomáš Vopat <tomas.vopat@gmail.com>
2018-12-04 21:13:33 +0100

    Přidání komentářů.

--- backend/src/main/java/ristaurace/repository/PolozkaMenuRepositoryCustom.java
+++ backend/src/main/java/ristaurace/repository/PolozkaMenuRepositoryCustom.java
@@ -8,5 +8,11 @@ import ristaurace.entities.TypPolozkaMenuEntity;
 import java.util.List;
 
 public interface PolozkaMenuRepositoryCustom {
+
+    /**
+     * Vrátí všechny TypPolozkaMenuEntity podle zvolené kategorie
+     * @param category_id
+     * @return
+     */
     List<TypPolozkaMenuEntity> findAllByCategory(Integer category_id);
 }
--- backend/src/main/java/ristaurace/repository/StulUcetRepositoryCustom.java
+++ backend/src/main/java/ristaurace/repository/StulUcetRepositoryCustom.java
@@ -7,7 +7,15 @@ import ristaurace.entities.StulUcetEntity;
 import java.util.List;
 
 public interface StulUcetRepositoryCustom {
+    /**
+     * Vrátí všechny položky se stavem otevřeno
+     * @return
+     */
     List<StulUcetEntity> findAllOpened();
 
+    /**
+     * Vrátí všechny položky se stavem zavřeno
+     * @return
+     */
     List<StulUcetEntity> findAllClosed();
 }
